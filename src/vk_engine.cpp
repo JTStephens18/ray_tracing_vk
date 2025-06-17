0a0548134d6dccce9d0a54617d3eb35716879bf4
JTStephens18 <JTStephens18@gmail.com>
2025-06-17 18:26:31 -0500

    initial commit

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -7,6 +7,8 @@
 #include <vk_initializers.h>
 #include <vk_types.h>
 
+#include "VkBootstrap.h"
+
 #include <chrono>
 #include <thread>
 
@@ -34,6 +36,61 @@ void VulkanEngine::init()
 
     // everything went fine
     _isInitialized = true;
+}
+
+void VulkanEngine::init_vulkan() {
+    vkb::InstanceBuilder builder;
+
+    auto inst_ret = builder.set_app_name("Vulkan Application")
+        .request_validation_layers(true)
+        .use_default_debug_messenger()
+        .require_api_version(1, 3, 0)
+        .build();
+
+    vkb::Instance vkb_inst = inst_ret.value();
+
+    // Grab the instance
+    _instance = vkb_inst.instance;
+    _debug_messenger = vkb_inst.debug_messenger;
+
+    SDL_Vulkan_CreateSurface(_window, _instance, &_surface);
+
+    VkPhysicalDeviceVulkan13Features features{ .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES };
+    features.dynamicRendering - true;
+    features.synchronization2 = true;
+
+    VkPhysicalDeviceVulkan12Features features12{ .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES };
+    features12.bufferDeviceAddress = true;
+    features12.descriptorIndexing = true;
+
+    vkb::PhysicalDeviceSelector selector{ vkb_inst };
+    vkb::PhysicalDevice physicalDevice = selector
+        .set_minimum_version(1, 3)
+        .set_required_features_13(features)
+        .set_required_features_12(features12)
+        .set_surface(_surface)
+        .select()
+        .value();
+
+    vkb::DeviceBuilder deviceBuilder{ physicalDevice };
+
+    vkb::Device vkbDevice = deviceBuilder.build().value();
+
+    _device = vkbDevice.device;
+    _chosenGPU = physicalDevice.physical_device;
+        
+}
+
+void VulkanEngine::init_swapchain() {
+
+}
+
+void VulkanEngine::init_commands() {
+
+}
+
+void VulkanEngine::init_sync_structures() {
+
 }
 
 void VulkanEngine::cleanup()
